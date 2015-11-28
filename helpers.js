dd0872ce53b69cabcef4dc55c3dbc9a095328399
libbyschuknight <libbysk@gmail.com>
2015-11-28 16:18:20 +1300

    Add random transparency on sqaures

--- helpers.js
+++ helpers.js
@@ -2,6 +2,10 @@ function rand8BitInt () {
   return Math.floor(Math.random() * 256);
 }
 
+function randPoint () {
+  return Math.random();
+}
+
 function randRGB () {
-  return 'rgb(' + rand8BitInt() + ',' + rand8BitInt() + ',' + rand8BitInt() + ')';
+  return 'rgb(' + rand8BitInt() + ',' + rand8BitInt() + ',' + rand8BitInt() + ',' + randPoint() + ')';
 }
